9a2327089f001177052eed38c878ba4e556676b3
costya27 <costik.t27@gmail.com>
2021-12-16 23:33:16 +0200

    +1

--- Number_inversion.py
+++ Number_inversion.py
@@ -1,2 +1,10 @@
-n = input()
-print(n - 1)
+n = int(input())
+n1 = int(input())
+s = 0
+while n > n1:
+    if n % 2 == 0:
+        n -= 1
+        print('-1')
+    elif n % 2 != 0:
+        n = n / 2
+        print(':2')
