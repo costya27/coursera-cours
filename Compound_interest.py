0e48b5ea43d19dcf1631a4c905f81cc3b46a0b8a
costya27 <costik.t27@gmail.com>
2022-01-17 20:18:15 +0200

    +-3

--- Compound_interest.py
+++ Compound_interest.py
@@ -1,9 +1,13 @@
-p = float(input())
+p = int(input())
 x = int(input())
 y = int(input())
 k = int(input())
-y2 = str('0.' + str(y))
-y3 = float(y2)
-n = x + y3
-sum1 = ((n * p) / 100) + n
-print(int(sum1), int(sum1 * 100 % 100))
+z = x * 100
+a = y + z
+n = 1
+while n <= k:
+    a = a + int(a * (p / 100))
+    n = n + 1
+b = a // 100
+c = a % 100
+print(b, c)
--- Quadratic_Equation_-2.py
+++ Quadratic_Equation_-2.py
@@ -6,9 +6,12 @@ d = (b**2)-(4*a*c)
 if d > 0:
     x1 = (-b + math.sqrt(d) / (2 * a))
     x2 = (-b - math.sqrt(d) / (2 * a))
-    print(x1, x2)
+    if x1 > x2:
+        print('2', x1, x2)
+    else:
+        print('2', x2, x1)
 elif d == 0:
     x1 = (-b + math.sqrt(d) / (2 * a))
-    print(x1)
+    print('1', x1)
 elif d < 0:
-    print('')
+    print('0')
